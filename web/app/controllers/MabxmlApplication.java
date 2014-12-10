f9b106bc0242aa4badc8247c05f918a164a5bf83
Fabian Steeg <fsteeg@gmail.com>
2014-12-10 11:43:19 +0100

    Extract some values to application.conf file, log ES GET URL

--- web/app/controllers/MabxmlApplication.java
+++ web/app/controllers/MabxmlApplication.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import java.io.File;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 import play.libs.F.Promise;
 import play.libs.ws.WS;
 import play.mvc.Controller;
@@ -14,16 +19,17 @@ import views.html.index;
  */
 public class MabxmlApplication extends Controller {
 
+	public static final Config CONFIG = ConfigFactory.parseFile(
+			new File("conf/application.conf")).resolve();
 	private static final String _SOURCE = "_source";
 	private static final String MABXML_ELASTICSEARCH = "mabxml-elasticsearch";
-	private static final String SERVER = "localhost";
+	private static final String SERVER = CONFIG.getString("es.server");
 	private static final String PORT = ":9200";
 	private static final String ES_SERVER = "http://" + SERVER + PORT;
-	private static final String ES_INDEX = "hbz01-test";
+	private static final String ES_INDEX = CONFIG.getString("es.index");
 	private static final String ES_TYPE = "mabxml";
 	private static final String GET_KEY = "mabXml";
 
-	
 	public static Result index() {
 		return ok(index.render(MABXML_ELASTICSEARCH));
 	}
@@ -36,6 +42,7 @@ public class MabxmlApplication extends Controller {
 		String url =
 				String.format("%s/%s/%s/%s/" + _SOURCE, ES_SERVER, ES_INDEX, ES_TYPE, id);
 		response().setContentType("text/xml");
+		play.Logger.info("Calling URL: " + url);
 		return WS.url(url).execute().map(x -> ok(x.asJson().get(GET_KEY).asText()));
 	}
 }
