3733b91a9011a8de4cd863626f177242f247be68
Pascal Christoph <christoph@hbz-nrw.de>
2025-02-20 12:26:42 +0100

    Remove HeapDumpOnOutOfMemoryError java option
    As there is a memory leak in metafacture-core#666 several instances
    dump 32 GB sized heap profiles. Our server have not enough space to
    handle these. It's btter to enable this option when needed, not as default.

--- monit_restart.sh
+++ monit_restart.sh
@@ -31,7 +31,7 @@ cd $HOME/git/$REPO
 case $ACTION in
 	start)
 		kill $(cat target/universal/stage/RUNNING_PID)
-		JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError" $HOME/activator-dist-1.3.5/activator "start $PORT"
+		JAVA_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError" $HOME/activator-dist-1.3.5/activator "start $PORT"
 		;;
 	stop)
 		kill $(cat target/universal/stage/RUNNING_PID)
